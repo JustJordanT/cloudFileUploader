21bcb2399d4cb7d441a8ce5cf6ffae35784c1edf
JustJordanT <jordantay9014@gmail.com>
2021-07-22 12:53:30 -0500

    adding files

---
+++ cloudFileUploader/Services/BlobService.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace cloudFileUploader.Services
+{
+    public class BlobService : IBlobService
+    {
+        public Task<string> GetBlob(string name, string containerName)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<IEnumerable<string>> AllBlobs(string containerName)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<bool> UploadBlob(string name, IFormFile file, string containerName)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<bool> DeleteBlob(string name, string containerName)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ cloudFileUploader/Startup.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Azure.Storage.Blobs;
+using cloudFileUploader.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace cloudFileUploader
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //Injecting the connection settings- from the app settings
+            var blobConnection = Configuration.GetValue<string>("BlobConnection");
+            
+            //injecting the blobserviceclient into out DI container.
+            services.AddSingleton(x => new BlobServiceClient(blobConnection));
+            
+            //Injecting the blob service into the DI
+            services.AddSingleton<IBlobService, BlobService>();
+            
+            
+            services.AddControllersWithViews();
+            
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
