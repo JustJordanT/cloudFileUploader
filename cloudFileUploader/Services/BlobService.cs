40abdbbbd6dbe1f9c545ca3d0f8ce563bf5582cc
JustJordanT <jordantay9014@gmail.com>
2021-07-22 13:26:06 -0500

    Finished creating the rest of the methods for the blob services.

--- cloudFileUploader/Services/BlobService.cs
+++ cloudFileUploader/Services/BlobService.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
 using Microsoft.AspNetCore.Http;
 
 namespace cloudFileUploader.Services
@@ -17,7 +18,13 @@ namespace cloudFileUploader.Services
 
         public async Task<string> GetBlob(string name, string containerName)
         {
-            throw new System.NotImplementedException();
+            //Allow us to access the data inside the container.
+            var containerClient = _blobClient.GetBlobContainerClient(containerName);
+            
+            //Get access to a certain file, inside the container, ie (FILENAME).
+            var blobClient = containerClient.GetBlobClient(name);
+
+            return blobClient.Uri.AbsoluteUri;
         }
 
         public async Task<IEnumerable<string>> AllBlobs(string containerName)
@@ -40,14 +47,36 @@ namespace cloudFileUploader.Services
             return files;
         }
 
-        public Task<bool> UploadBlob(string name, IFormFile file, string containerName)
+        public async Task<bool> UploadBlob(string name, IFormFile file, string containerName)
         {
-            throw new System.NotImplementedException();
+            //Allow us to access the data inside the container.
+            var containerClient = _blobClient.GetBlobContainerClient(containerName);
+            
+            //Checking if file exists, if not a temp space untill it is uploaded.
+            var blobClient = containerClient.GetBlobClient(name);
+
+            var httpHeaders = new BlobHttpHeaders()
+            {
+                ContentType = file.ContentType
+            };
+
+            var res = await blobClient.UploadAsync(file.OpenReadStream(), httpHeaders);
+
+            if (res is not null)
+                    return true;
+            return false;
         }
 
-        public Task<bool> DeleteBlob(string name, string containerName)
+        public async Task<bool> DeleteBlob(string name, string containerName)
         {
-            throw new System.NotImplementedException();
+            //Allow us to access the data inside the container.
+            var containerClient = _blobClient.GetBlobContainerClient(containerName);
+            
+            //Get access to a certain file, inside the container, ie (FILENAME).
+            var blobClient = containerClient.GetBlobClient(name);
+            
+            //if the file exists the file wi;; be deleted.
+            return await containerClient.DeleteIfExistsAsync();
         }
     }
 }
